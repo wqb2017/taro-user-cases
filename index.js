e74fb7a92e9280d566d4ea76feafa1d6ffc63550
wangqibiao <18354270712@163.com>
2019-11-29 15:13:30 +0800

    Update index.js
    add 微信小程序案例【绘管家】【 绘服务】

--- index.js
+++ index.js
@@ -487,6 +487,14 @@ const userCases = {
     {
       name: "甩甩宝宝",
       url: "https://camo.githubusercontent.com/8f0cec6cae8b210c496239f731f0598d0afd5390/68747470733a2f2f77782e73716d69616f3030312e636f6d2f6173737365742f7a7567616e672f62616f62616f2f636f64655f68352e6a7067"
+    },
+    {
+      name: "绘管家",
+      url: "https://github.com/wqb2017/wisdom-city/blob/master/images/huiguanjia.jpg"
+    },
+    {
+      name: "绘服务",
+      url: "https://github.com/wqb2017/wisdom-city/blob/master/images/huifuwu.jpg"
     }
   ],
   H5: [
